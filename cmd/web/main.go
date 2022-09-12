9f4e8053693a9455e540d6a4f63896d36d642a5f
lukasz0707 <lukaszs0707@gmail.com>
2022-09-12 18:15:09 +0200

    Run time flag for port number and improved logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of
+	// and some short help text explaining what the flag controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Importantly, we use the flag.Parse() function to parse the command-line
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any er
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+	// Use log.New() to create a logger for writing information messages. This
+	// three parameters: the destination to write the logs to (os.Stdout), a st
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the fl
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// Create a logger for writing error messages in the same way, but use stde
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -18,7 +41,11 @@ func main() {
 	// all URL paths that start with "/static/". For matching paths, we strip t
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// The value returned from the flag.String() function is a pointer to the fl
+	// value, not the value itself. So we need to dereference the pointer (i.e.
+	// prefix it with the * symbol) before using it.
+	// Write messages using the two new loggers, instead of the standard logger
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
