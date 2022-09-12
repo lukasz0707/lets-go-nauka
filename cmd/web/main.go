f96fc7b30e8bc6f45b79d38c48e806cedd8bcc51
lukasz0707 <lukaszs0707@gmail.com>
2022-09-12 20:09:50 +0200

    creating routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,36 +22,16 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Initialize a new instance of application containing the dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	// Swap the route declarations to use the application struct's methods as t
-	// handler functions.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-	// Create a file server which serves files out of the "./ui/static" directo
-	// Note that the path given to the http.Dir function is relative to the pro
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip t
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Initialize a new http.Server struct. We set the Addr and Handler fields
-	// that the server uses the same network address and routes as before, and
-	// the ErrorLog field so that the server now uses the custom errorLog logge
-	// the event of any problems.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	// Call the ListenAndServe() method on our new http.Server struct.
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
