25ee37fd7fa4cd1aee995026e08c1ff77b1bdf0b
lukasz0707 <lukaszs0707@gmail.com>
2022-09-16 15:37:18 +0200

    added routing library bmizerany/pat

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,10 +11,6 @@ import (
 // Change the signature of the home handler so it is defined as a method agains
 // *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -30,7 +26,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 // Change the signature of the showSnippet handler so it is defined as a method
 // against *application.
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w) // Use the notFound() helper.
 		return
@@ -51,14 +47,14 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 // Change the signature of the createSnippet handler so it is defined as a metho
 // against *application.
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
-		return
-	}
+
 	// Create some variables holding dummy data. We'll remove these later on
 	// during the build.
 	title := "O snail"
@@ -73,5 +69,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Redirect the user to the relevant page for the snippet.
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
